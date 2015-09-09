a31b8aa5388cb299312bd56029f7042ff28cd29c
dpzmick <dpzmick@gmail.com>
2015-09-09 00:35:12 -0500

    format test

--- test/set_clj/core_test.clj
+++ test/set_clj/core_test.clj
@@ -4,49 +4,39 @@
 
 (deftest test-check-single-attr-same
   (testing "does it recognize all same attribute"
-    (is (check-single-attribute :color [
+    (is (check-single-attribute :color [{:color :red}
                                         {:color :red}
-                                        {:color :red}
-                                        {:color :red}
-                                        ]))))
+                                        {:color :red}]))))
 
 (deftest test-check-single-attr-diff
   (testing "does it recognize all different attribute"
-    (is (check-single-attribute :color [
-                                        {:color :red}
+    (is (check-single-attribute :color [{:color :red}
                                         {:color :green}
-                                        {:color :bleu}
-                                        ]))))
+                                        {:color :bleu}]))))
 
 (deftest test-check-single-attr-almost-diff
   (testing "does it recognize almost all different attribute"
-    (is (not (check-single-attribute :color [
-                                        {:color :red}
-                                        {:color :red}
-                                        {:color :bleu}
-                                        ])))))
+    (is (not (check-single-attribute :color [{:color :red}
+                                             {:color :red}
+                                             {:color :bleu}])))))
 
 (deftest test-check-single-attr-not-def
   (testing "does it return false when one of the cards doesn't have the right elements?"
-    (is (not (check-single-attribute :color [
-                                        {:color :red}
-                                        {:shape :squiggle}
-                                        {:color :bleu}
-                                        ])))))
+    (is (not (check-single-attribute :color [{:color :red}
+                                             {:shape :squiggle}
+                                             {:color :bleu}])))))
+
 (deftest is-set
   (testing "does it recognize a valid set"
-        (is (is-set? [
-                      {:color :red, :shape :squiggle, :number 1}
+        (is (is-set? [{:color :red, :shape :squiggle, :number 1}
                       {:color :red, :shape :squiggle, :number 2}
-                      {:color :red, :shape :squiggle, :number 3}
-                      ]))))
+                      {:color :red, :shape :squiggle, :number 3}]))))
+
 (deftest is-not-set
   (testing "does it recognize a valid set"
-        (is (not (is-set? [
-                      {:color :red, :shape :oval,     :number 2}
-                      {:color :red, :shape :squiggle, :number 2}
-                      {:color :red, :shape :squiggle, :number 3}
-                      ])))))
+        (is (not (is-set? [{:color :red, :shape :oval,     :number 2}
+                           {:color :red, :shape :squiggle, :number 2}
+                           {:color :red, :shape :squiggle, :number 3}])))))
 
 (deftest is-valid-attrs
   (testing "does it recognize valid attribute map"
